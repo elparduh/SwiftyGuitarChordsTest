4542f172c1fa362368f44d731fb4641f6d1c37aa
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2024-12-22 16:25:29 -0600

    first commit

--- TestChord/ViewController.swift
+++ TestChord/ViewController.swift
@@ -1,19 +1,26 @@
-//
-//  ViewController.swift
-//  TestChord
-//
-//  Created by Canelito on 22/12/24.
-//
-
 import UIKit
+import SwiftyChords
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var chordImg: UIImageView!
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    testChords()
+    getChordImage()
   }
 
+  func testChords() {
+    let chordPosition = Chords.guitar.matching(key: Chords.Key(rawValue: "C")!).matching(suffix: .major).first!
+    let frame = CGRect(x: 0, y: 0, width: 100, height: 150) // I find these sizes to be good.
+    let layer = chordPosition.chordLayer(rect: frame, chordName: .init(show: true, key: .symbol, suffix: .symbolized), forPrint: false)
+    chordImg.image = layer.image() // might be expensive. Use Layers when possible while drawing to a view. Images are better if you plan to send them outside the app.
+  }
+
+  func getChordImage() {
+    let data = Chords.guitar.count
+    print(data)
+  }
 
 }
 
